f6c4392c946f161c2b65825007f20df1c2731f4f
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-05-22 23:27:44 -0300

    styles and animation from search results

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import '../styles/Buscador.css'
 import { useCityTrieSearch } from '../hooks/useCitieTrieSearch'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function Buscador ({ setCity, trie }) {
   const [query, setQuery] = useState('')
@@ -12,26 +13,50 @@ export default function Buscador ({ setCity, trie }) {
     setCity(query)
   }
 
+  const handleClickResult = city => {
+    setCity(city.name)
+    setQuery('')
+  }
+
   return (
     <form action='get' className='buscador' onSubmit={e => handleSubmit(e)}>
-      <div>
+      <div className='buscador-container'>
         <input
           type='text'
           onChange={e => setQuery(e.target.value)}
           placeholder='London, Paris, Rome'
           className='input-buscador'
+          value={query}
         />
         <button type='submit' className='btn-buscador'>
           Search
         </button>
       </div>
-      <ul>
-        {results.slice(0, 6).map((city, idx) => (
-          <li key={idx} onClick={e => setCity(city.name)}>
-            {city.name}
-          </li>
-        ))}
-      </ul>
+
+      <AnimatePresence>
+        {results.length > 0 && (
+          <motion.ul
+            className='results'
+            initial={{ opacity: 0, maxHeight: 0 }}
+            animate={{ opacity: 1, maxHeight: 350 }}
+            exit={{ opacity: 0, maxHeight: 0 }}
+            transition={{ duration: 0.4 }}
+          >
+            {results.slice(0, 6).map((city, idx) => (
+              <motion.li
+                key={idx}
+                onClick={() => handleClickResult(city)}
+                initial={{ opacity: 0, x: -20 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 20 }}
+                transition={{ duration: 0.6, delay: idx * 0.2 }}
+              >
+                {city.name}
+              </motion.li>
+            ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </form>
   )
 }
